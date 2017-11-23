4cb13ee986486b740d65f905e45155b771db16bf
krolim <chaosfire@gmail.com>
2017-11-24 01:25:23 +0200

    dev

--- react/src/App.js
+++ react/src/App.js
@@ -2,26 +2,33 @@ import React, { Component } from 'react';
 // import './App.css';
 import Client from "./Client";
 import { Route } from 'react-router'
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Redirect } from 'react-router-dom';
 import Moderator from './Moderator';
 import Register from './Register';
 import Voter from "./Voter";
+import { MediaQuery } from 'react-responsive';
 
 class App extends Component {
- 
+
+  componentWillMount(){
+    this.setState({width: window.innerWidth});
+  }
+  
+
   render() {
+      const redirect = this.state.width > 1000 ? '/admin':'/reg';
       return (
-        
       <div style={{ "width": "100%" }}>
-      <BrowserRouter>
-        <div>
-            <Switch>
-              <Route path="/reg" component={Register}/>
-              <Route path="/voting" component={Voter}/>
-              <Route path="/admin" component={Moderator} /> }/>
-            </Switch>
-        </div>
-      </BrowserRouter>
+        <BrowserRouter>
+          <div>
+              <Switch>
+                <Route path="/reg" component={Register}/>
+                <Route path="/voting" component={Voter}/>
+                <Route path="/admin" component={Moderator}/> 
+              </Switch>
+              <Redirect to={redirect} />
+          </div>
+        </BrowserRouter>
     </div>
     );
   }
