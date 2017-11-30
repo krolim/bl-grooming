a5c0d3bccb1690f73e849ee90a4c1486f2552025
krolim <chaosfire@gmail.com>
2017-12-01 00:50:29 +0200

    dev

--- react/src/Moderator.js
+++ react/src/Moderator.js
@@ -19,19 +19,23 @@ class Moderator extends Component {
       },
       voteStats: {
         min: -1,
-        max: 1000
+        max: 1000,
+        weights: [],
+        allVoters: 0,
+        avg: 0,
+        decision: false
       },
       showVotes: false,
       votingFinished: false, 
       view: 'all'
     };
-    this.retrieveVoters();
     this.handleVoteClick = this.handleVoteClick.bind(this);
     this.handleNewVoteClick = this.handleNewVoteClick.bind(this);
     this.footer = this.footer.bind(this);
   } 
 
   componentDidMount() {
+    this.retrieveVoters();
     this.interval = setInterval(this.retrieveVoters.bind(this), 1000);
   }
 
@@ -56,14 +60,40 @@ class Moderator extends Component {
         voter.status !== 'voted');
       if (voters.voted.length > 1) {
         this.setState({
-          voteStats: { 
-            min: voters.voted[0].vote, 
-            max: voters.voted[voters.voted.length-1].vote
-          }
+          voteStats: this.getStats(voters)
         });
       }
       this.setState({ voters: voters });
     });
+  }
+
+  getStats(voters) {
+    const allVoters = voters.all.length;
+    const weights = [];
+    for (let i=0; i<allVoters; ++i)
+      weights.push(0);
+    const stats = {
+      allVoters: allVoters,
+      decision: false,
+      weights: weights,
+      avg: 0,
+      min: voters.voted[0].vote, 
+      max: voters.voted[voters.voted.length-1].vote
+    };
+    stats.decision = stats.min === stats.max && stats.min > 0;
+    if (stats.decision) {
+      stats.avg = stats.min;
+      stats.weights[stats.min] = allVoters;
+    } else {
+      let sum = 0;
+      voters.all.forEach(voter => {
+        const vote = Number.parseInt(voter.vote);
+        sum +=  vote;
+        stats.weights[vote] += 1;
+      });
+      stats.avg = sum/allVoters;
+    }
+    return stats;
   }
 
   handleVoteClick() {
@@ -87,6 +117,22 @@ class Moderator extends Component {
         showVotes: false
       });
     });
+  }
+
+  voteStats(stats) {
+    const weights = [];
+    stats.weights.forEach((weight, index) => {
+      if (weight > 0) {
+        const percent = weight/stats.allVoters*100;
+        weights.push(
+          <span>{ index + ' (' + percent + '%' + ')'}</span>);
+      }
+    });
+    return (
+      <div className="voteStats">
+        Avg: {stats.avg.toFixed(2)}, Min: {stats.min}, Max: {stats.max} -- {weights}
+      </div>
+    );
   }
 
   footer() {
@@ -132,11 +178,17 @@ class Moderator extends Component {
 
   render() {
     const view = this.state.view === 'all'?this.state.voters.all:this.state.voters.voted;
+    const body = this.state.voteStats.decision ? 
+      <div className="voteDecision"></div> : 
+      <div>
+        <VoterQueue voters={view} showVotes={this.state.showVotes} voteStats={this.state.voteStats}  />;
+          { this.state.showVotes?this.voteStats(this.state.voteStats):'' }
+      </div>
     return (
       <div>
         { this.nav() }
         <div className="container" style={{"width": "100%", "textAlign": "center"}}>
-          <VoterQueue voters={view} showVotes={this.state.showVotes} voteStats={this.state.voteStats}  />
+          { body }
           { this.footer() }
         </div>
       </div>      
